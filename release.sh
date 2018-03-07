dd54421c550c14dcbea6a502142acdde3199a362
Jaroslaw Bochniak <j.bochniak@smartrecruiters.com>
2018-03-07 17:54:43 +0100

    Release script update

--- release.sh
+++ release.sh
@@ -3,7 +3,7 @@
 # export GITHUB_TOKEN=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 # https://goreleaser.com/#quick_start
 
-git tag -a v -m "${1} Release"
+git tag -a v${1} -m "${1} Release"
 git push origin v${1}
 
-goreleaser --snapshot --rm-dist
+goreleaser --rm-dist
