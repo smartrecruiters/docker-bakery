be3d560095538670404444942d831b32e74e7e21
Tomasz Lesniewski <t.lesniewski@smartrecruiters.com>
2024-05-30 11:37:58 +0200

    Add tests for parsing FROM

---
+++ bakery/service/parser_test.go
@@ -0,0 +1,41 @@
+package service
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func copyDockerImgObjectForCompare(dockerImg *DockerImage) *DockerImage {
+	return &DockerImage{
+		Name:             dockerImg.Name,
+		DockerfilePath:   "dummy",
+		DockerfileDir:    dockerImg.DockerfileDir,
+		DependsOnLong:    dockerImg.DependsOnLong,
+		DependsOnShort:   dockerImg.DependsOnShort,
+		DependsOnVersion: dockerImg.DependsOnVersion,
+		nextVersion:      dockerImg.nextVersion,
+		latestVersion:    dockerImg.latestVersion,
+	}
+}
+
+func TestParserDependenciesFromFromClause(t *testing.T) {
+	dockerImgParser := NewDockerImageParser()
+
+	dockerImgUnnamed, _ := dockerImgParser.ParseDockerfile("testcases/Dockerfile1")
+	dockerImgUnnamedCopy := copyDockerImgObjectForCompare(dockerImgUnnamed)
+
+	dockerImgNamedUppercase, _ := dockerImgParser.ParseDockerfile("testcases/Dockerfile2")
+	dockerImgNamedUppercaseCopy := copyDockerImgObjectForCompare(dockerImgNamedUppercase)
+
+	dockerImgNamedLowercase, _ := dockerImgParser.ParseDockerfile("testcases/Dockerfile3")
+	dockerImgNamedLowercaseCopy := copyDockerImgObjectForCompare(dockerImgNamedLowercase)
+
+	assert.Equal(t, dockerImgUnnamed.DependsOnLong, "ubuntu:2204")
+	assert.Equal(t, dockerImgUnnamed.DependsOnShort, "ubuntu")
+	assert.Equal(t, dockerImgUnnamed.DependsOnVersion, "2204")
+
+	assert.Equal(t, dockerImgUnnamedCopy, dockerImgNamedUppercaseCopy)
+	assert.Equal(t, dockerImgUnnamedCopy, dockerImgNamedLowercaseCopy)
+	assert.Equal(t, dockerImgNamedUppercaseCopy, dockerImgNamedLowercaseCopy)
+}
